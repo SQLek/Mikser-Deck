f4714f8536c3c3a1254472df9e8d4b5b1983ab74
Nicus <nicus101@gmail.com>
2024-04-11 20:49:30 +0200

    volume level indicators with mockup data done graphical bugfixes
    Co-authored-by: Piotr SQLek Skólski <github@sqlek.org>

--- src/display.cpp
+++ src/display.cpp
@@ -8,26 +8,26 @@
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 int8_t points[] = {
-    0, 16,
-    6, 15,
-    11, 13,
-    14, 8,
-    16, 3,
-    16, -3,
-    14, -8,
-    11, -13,
-    -6, -15,
+    0, 17,
+    6, 16,
+    11, 14,
+    15, 9,
+    17, 3,
+    17, -3,
+    15, -9,
+    11, -14,
+    6, -16,
+    1, -17,
     -6, -16,
-    -11, -15,
-    -14, -13,
-    -16, -8,
-    -16, -3,
-    -14, 3,
-    -11, 8,
-    -6, 13,
-    0, 15,
-    0, 16, // fake entry - double of 0 index
-};
+    -11, -14,
+    -15, -9,
+    -17, -3,
+    -17, 3,
+    -15, 9,
+    -11, 14,
+    -6, 16,
+    0, 17, // fake entry - double of 0 index
+    6, 16};
 
 static uint8_t animation;
 
@@ -45,30 +45,58 @@ void setupScreen()
 
 void drawCircleIndicator(uint8_t x, uint8_t y, uint8_t size, uint16_t volume, char c)
 {
+    volume = volume / 64;
     oled.fillCircle(x, y, size, WHITE);
-    oled.fillCircle(x, y, size/2, BLACK);
+    oled.fillCircle(x, y, size / 2, BLACK);
+
+    for (uint8_t i = 0; i < volume; i++)
+    {
+        uint8_t j = i + 1;
+        oled.fillTriangle(
+            x, y,
+            x + points[j * 2], y + points[j * 2 + 1],
+            x + points[j * 2 + 2], y + points[j * 2 + 3],
+            BLACK);
+    }
+
+    oled.drawCircle(x, y, size + 1, WHITE);
+    oled.drawCircle(x, y, size / 2, WHITE);
 
     oled.fillTriangle(
-        x,y,
-        x+points[animation],y+points[animation+1],
-        x+points[animation+2],y+points[animation+3],
-        BLACK
-    );
+        x, y,
+        x + points[36], y + points[36 + 1] + 1,
+        x + points[38], y + points[38 + 1] + 1, // pierwsza linia
+        BLACK);
 
-    oled.drawChar(x-2, y-3, c, WHITE, BLACK, 1);
+    oled.fillTriangle(
+        x, y,
+        x + points[36], y + points[36 + 1] + 1,
+        x + points[34], y + points[34 + 1] + 1, // druga linia
+        BLACK);
+    oled.drawLine(
+        x + points[38] / 2, y + points[38 + 1] / 2,
+        x + points[38] - 1, y + points[38 + 1] - 1,
+        WHITE);
+    oled.drawLine(
+        x + points[34] / 2, y + points[34 + 1] / 2,
+        x + points[34] - 1, y + points[34 + 1] - 1,
+        WHITE);
+
+    oled.drawChar(x - 2, y - 3, c, WHITE, BLACK, 1);
 }
 
 void testScreen()
 {
-    drawCircleIndicator(24,47,16,512,'3');
-    drawCircleIndicator(64,47,16,512,'4');
-    drawCircleIndicator(104,47,16,512,'5');
-    drawCircleIndicator(44,17,16,512,'1');
-    drawCircleIndicator(84,17,16,512,'2');
-    
+    drawCircleIndicator(24, 47, 16, animation * 64, '3');
+    drawCircleIndicator(64, 47, 16, animation * 64, '4');
+    drawCircleIndicator(104, 47, 16, animation * 64, '5');
+    drawCircleIndicator(44, 17, 16, animation * 64, '1');
+    drawCircleIndicator(84, 17, 16, animation * 64, '2');
+
     oled.display();
-    animation += 2;
-    if (animation >= 36) {
+    animation++;
+    if (animation >= 16)
+    {
         animation = 0;
     }
 
